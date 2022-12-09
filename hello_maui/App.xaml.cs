12fc7a94944e0101315b3e47f4ea063089daa62b
Phat <damphat@gmail.com>
2022-12-09 16:06:52 +0700

    init

---
+++ hello_maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace hello_maui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ hello_maui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace hello_maui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
